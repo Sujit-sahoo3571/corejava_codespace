e010135db07d602681232009e1ba320a155b79b8
Sujit <55569677+Sujit-sahoo3571@users.noreply.github.com>
2023-11-05 12:35:22 +0000

    fibbonacci series

---
+++ core_java/Fibbonacci.java
@@ -0,0 +1,16 @@
+public class Fibbonacci {
+
+    public static void main(String[] args) {
+        int a =0   , b =1 , c  =0 ; 
+
+        for(int i = 0 ; i< 10 ; i++ ){
+            System.out.print(c+ " ");
+            a = b ; 
+            b = c ; 
+            c = a+b ; 
+
+        }
+
+    }
+    
+}
